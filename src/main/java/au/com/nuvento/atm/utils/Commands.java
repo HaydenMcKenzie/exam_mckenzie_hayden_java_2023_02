63fe66b2aab0a78d29e89ff0c2999f50e65504b9
HaydenMcKenzie <Haydendanielmckenzie@gmail.com>
2023-10-28 22:13:00 +1100

    I added a test case for Commands.

--- src/main/java/au/com/nuvento/atm/utils/Commands.java
+++ src/main/java/au/com/nuvento/atm/utils/Commands.java
@@ -1,15 +1,13 @@
 package au.com.nuvento.atm.utils;
 
 
-import au.com.nuvento.atm.ATM;
 import au.com.nuvento.atm.accounts.BankAccount;
 import au.com.nuvento.atm.accounts.BankBuilder;
-
-import java.io.FileNotFoundException;
 
 import static au.com.nuvento.atm.ATM.bankAccount;
 
 public class Commands {
+    BankBuilder account = new BankBuilder();
     /**
      * New Balance Class allows you to input an amount and add/minus from existing account
      * @param userName is user input and is either 001, 002 or 003 - for userID
@@ -18,10 +16,8 @@ public class Commands {
      * @param userAmount is user input and presents what is needed to either be added or taken away
      * @return new balance or old balance                          - old balance is only if there is an input error
      */
-    public double newBalance(String userName, String options, String userOption, double userAmount) throws FileNotFoundException {
-        BankBuilder selectAccount = new BankBuilder();
-
-        String grabAccountInfo = selectAccount.selectAccount(userName, options);
+    public double newBalance(String userName, String options, String userOption, double userAmount) {
+        String grabAccountInfo = account.selectAccount(userName, options);
         BankAccount b = bankAccount.get(grabAccountInfo);
 
         double balance = Double.parseDouble(b.getOpeningBalance());
@@ -31,10 +27,6 @@ public class Commands {
                 return balance + userAmount;
             case "2":
                 return balance - userAmount;
-            default:
-                System.out.println("ERROR: You didn't enter a number");
-                ATM.setup();
-                ATM.userInteraction();
         }
         return balance;
     }
@@ -46,8 +38,6 @@ public class Commands {
      * @return Opening Balance of selected account
      */
     public String newWriteBal(String userName, String options) {
-        BankBuilder account = new BankBuilder();
-
         String grabAccountInfo = account.selectAccount(userName, options);
         BankAccount b = bankAccount.get(grabAccountInfo);
 
---
+++ src/test/java/au/com/nuvento/atm/utilstest/CommandsTest.java
@@ -0,0 +1,43 @@
+package au.com.nuvento.atm.utilstest;
+
+import au.com.nuvento.atm.ATM;
+import au.com.nuvento.atm.accounts.BankAccount;
+import au.com.nuvento.atm.utils.Commands;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static au.com.nuvento.atm.ATM.bankAccount;
+
+public class CommandsTest {
+    Commands c;
+    BankAccount bal;
+
+
+    @BeforeEach
+    public void setup() throws FileNotFoundException {
+        ATM.setup();
+        c = new Commands();
+        bal = bankAccount.get("9264945");
+    }
+
+    @Test
+    public void testNewBalanceForAdding() {
+        double addResult = c.newBalance("001", "1","1", 10);
+        assertEquals(addResult, 510.09);
+    }
+    @Test
+    public void testNewBalanceForMinus() {
+        double minusResult = c.newBalance("001", "1","2", 10);
+        assertEquals(minusResult, 490.09);
+    }
+
+    @Test
+    public void TestNewWrittenBalanceForJohnsFirstAccount() {
+        String result = c.newWriteBal("001", "1");
+        assertEquals(result, bal.getOpeningBalance());
+    }
+}
